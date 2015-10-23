2e0cfef424edcd7a2dc2aca2ae78bc0b87c26c7a
Derek White <derekwhite199@gmail.com>
2015-10-23 12:03:47 -0400

    changed to include a loop

--- Accelerometer.py
+++ Accelerometer.py
@@ -2,29 +2,24 @@ from random import randint
 
 
 def stubAccelerometer():
+	
 	thistime = randint(0,50)
+	string = ""
+	
 	if thistime>45:
 		return "garbage"
 	elif thistime > 40:
-		xf = randint(100,500)
-		yf = randint(100,500)
-		zf = randint(100,500)
-		xa = randint(100,900)
-		ya = randint(100,900)
-		za = randint(100,900)
+		min = 100
+		max = 500
 	elif thistime <=7:
-		xf = randint(0,-500)
-		yf = randint(0,-500)
-		zf = randint(0,-500)
-		xa = randint(0,-900)
-		ya = randint(0,-900)
-		za = randint(0,-900)
+		min = -500
+		max = 0
 	else
-		xf = randint(0,50)
-		yf = randint(0,50)
-		xa = randint(0,90)
-		ya = randint(0,50)
-		zf = randint(0,90)
-		za = randint(0,90)
-	string = "" + xa + ya + za + xf + yf + zf
+		min = 0
+		max = 50
+	
+	for i in range(0, 5):
+		string += randint(min, max) + " "
+	
+	
 	return string
