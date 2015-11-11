5aba42c6565425aa4c3507f33f439136c5b9fd27
amrgawish <amrala94@hotmail.com>
2015-11-11 03:01:24 +0000

    second edition of the converter with pulse

--- ATD-MAIN/ADConverter.py
+++ ATD-MAIN/ADConverter.py
@@ -8,15 +8,36 @@ def bitstring(n):
 def read (adc_channel=0 , spi_channel=0):
 	conn = spidev.SpiDev(0,spi_channel)
 	conn.max_speed_hz = 1200000
-	cmd = 128
+	conn.mode = 0
+	cmd =192 
 	if adc_channel:
 		cmd+=32
 	reply_bytes= conn.xfer2([cmd,0])
 	reply_bitstring = ''.join(bitstring(n) for n in reply_bytes)
 	reply = reply_bitstring[5:15]
-	return int(reply,2)
+	return int(reply,2)/ 2**10
 
 if __name__ == '__main__':
-	for i in range (0,50):
-		print read()
-		time.sleep (0.25)
+	count = 0
+#	for x in range (0,100):
+#		print read()
+#		time.sleep(0.2)
+	
+	while True:
+		time.sleep(0.1)
+		if read() > 0.65:
+			count = count + 1
+			print "     ---"
+			print "    ----------------"
+			print "      ----------------------"
+			print "      ----------------------------- %d" % count
+			print "     ----------------------"
+			print "    ------------"
+			print "     ---"
+			print "     --------"
+			print "      ---------"
+			print "     ------"
+			print "     ---"
+		elif read() < 0.5:
+			print "     ---  %.10f "%read()
+                       
