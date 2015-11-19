82999ba88b68581687c8193541da88ef31cca5bd
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2015-11-18 20:02:03 -0500

    Moved model creation out of controller and into the main exe

--- ServerStandAlone/runServer.py
+++ ServerStandAlone/runServer.py
@@ -3,13 +3,17 @@ import sys
 sys.path.append(os.getcwd()+'/server')
 sys.path.append(os.getcwd()+'/lib')
 import serverCtrl
+import serverModl
 
 def main():
 	DatabasePath = 'lifeBandDB.db'
-	ListenIP ='0.0.0.0' #'172.17.148.20'
-	receivePort =5005
-	sendPort = 6006
-	controller = serverCtrl.serverController(DatabasePath,ListenIP,receivePort,sendPort)
+	ListenIP ='172.17.158.44'#'0.0.0.0' #'172.17.148.20'
+	Port = 5005
+	#receivePort =5005
+	#sendPort = 6006
+	model = serverModl.serverModel(DatabasePath)
+	model.createSensorDatabase()
+	controller = serverCtrl.serverController(DatabasePath,ListenIP,Port,model) #receivePort,sendPort)
 	controller.runServer()
 
 
