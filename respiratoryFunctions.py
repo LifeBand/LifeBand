28e55263aab14e9edf33c5ed6a4bfa6fb05821ca
Derek-a-white <derekwhite199@gmail.com>
2015-11-19 12:19:42 -0500

    Changed variable name

--- respiratoryFunctions.py
+++ respiratoryFunctions.py
@@ -1,19 +1,19 @@
 import math
 amplitude = 1
 vShift = 2
-period = 0.0004*math.pi()
+frequency = 0.0004*math.pi()
 def bradypnea_resp(count):
-    thing = 2*amplitude*math.sin(period*count/2)
+    thing = 2*amplitude*math.sin(frequency*count/2)
     if thing < 0:
         return 0
     else:
         return thing + vShift
         
 def cheyne_Stokes_resp(count):
-    return amplitude*math.sin(period*count)*math.sin(period*count/3) +vShift
+    return amplitude*math.sin(frequency*count)*math.sin(frequency*count/3) +vShift
 
 def kussmauls_resp(count):
-    return 1.25*amplitude*math.sin(period*count*2) + vShift
+    return 1.25*amplitude*math.sin(frequency*count*2) + vShift
 
 def apneustic_resp(count):
-    return abs(math.sin(period*count)) + vShift
+    return abs(math.sin(frequency*count)) + vShift
