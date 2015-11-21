d4b445a0eef0a01af0892b24a65b984cbc6c4b60
amrgawish <amrala94@hotmail.com>
2015-11-21 01:28:11 +0000

    pulse

--- ATD-MAIN/newpulseAD.py
+++ ATD-MAIN/newpulseAD.py
@@ -26,15 +26,22 @@ def read (adc_channel=0 , spi_channel=0):
 #gets a ratio of the pulse per min (freq) and it saves in an array and sends the average ever new data entered
 # if there was no pulse it sends the signals envoking the allarm to start 
 def arraythread ():
-    global beat_num
-    global timeStamp
-    BPM = []*10
-    if (beat_num%60 != 0): 
-        for i in range (1,beat_num):
-            diff = timeStamp[i%60]-timeStamp[i%60]
-            if diff >= 0.4:
-                BPM [i%10] = (1/diff)*60
-                time.sleep(1)
+    i = 1 
+#    global beat_num
+ #   global timeStamp
+    while True:
+       time.sleep (1)
+       print ("threadss %d",timeStamp[beat_num%60] )
+       BPM = [0]*10
+       if (beat_num%60 != 0):
+   	   #print "in first"
+           diff = timeStamp[i%60] - timeStamp[(i%60)-1]
+	   #print ("in for loop %d",diff)
+           if diff >= 0.4:
+               BPM [i%10] = (1/diff)*60
+	       print ("thread %d",BPM[i%10])
+	       i = i +1
+   
                 
 # it starts the thread every time needed
 def start ():
@@ -45,15 +52,19 @@ def start ():
 
 threads = []
 beat_num = 0
-timeStamp = []*60
+timeStamp = [0]*60
 
 if __name__ == '__main__':
+    global beat_num
+    global timeStamp
     start()
     while True:
+	time.sleep(0.2)
         beat = read()
-        time = time.time()
-        if (beat_num >= 0.65):
-            timeStamp[beat_num%60] = time                
-            beat_num = beat_num + 1
-            
+        timee = time.time()
+        if (beat >= 0.65):
+            timeStamp[beat_num%60] = timee                
+            print ("main %d",timeStamp[beat_num%60])
+	    beat_num = beat_num + 1
+	                
                 
