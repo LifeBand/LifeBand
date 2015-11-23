fda18353feeefc21e730c7325d9c1df8c8485763
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2015-11-22 19:53:06 -0500

    recording voltages into excel spreadsheet

---
+++ PythontoExcelVoltages/src/pythontoexcelvoltages.py
@@ -0,0 +1,43 @@
+import xlwt
+from datetime import datetime
+from __future__ import division 
+import spidev 
+import time
+
+__author__ = "dominikschmidtlein"
+__date__ = "$Nov 22, 2015 7:35:40 PM$"
+
+def bitstring(n):
+	s=bin(n)[2:]
+	return '0'*(8-len(s))+s
+
+def read (adc_channel=0 , spi_channel=0):
+	conn = spidev.SpiDev(0,spi_channel)
+	conn.max_speed_hz = 1200000
+	conn.mode = 0
+	cmd =192 
+	if adc_channel:
+		cmd+=32
+	reply_bytes= conn.xfer2([cmd,0])
+	reply_bitstring = ''.join(bitstring(n) for n in reply_bytes)
+	reply = reply_bitstring[5:15]
+	conn.close()
+	return int(reply,2)/ 2**10
+
+if __name__ == "__main__":    
+    
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('voltages vs time')
+    
+    row = 1
+    t1 = time.time()
+    while abs(time.time() - t1)  < 10:
+        t2 = time.time()
+        time = t2 - t1
+        voltage = read()
+        ws.write(0, row, time)
+        ws.write(1, row, voltage)
+        row += 1
+    wb.save('voltages.xls')
+        
+        
