3f4c8a54f649d350d5b0180e57cefcb5996012cb
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2015-11-26 13:58:11 -0500

    fixed sqrt call, added sleep period

--- AccelerometerMeasurements/src/accelerometermeasurements.py
+++ AccelerometerMeasurements/src/accelerometermeasurements.py
@@ -1,15 +1,17 @@
 from adxl345 import ADXL345
 from math import sqrt
+from time import sleep
 
 __author__ = "dominikschmidtlein"
 __date__ = "$Nov 26, 2015 1:12:15 PM$"
 
 MAGNITUDE_THRESHOLD = 2.5
+PERIOD_BETWEEN_SAMPLES = 0.01
 
 def get_magnitude(x, y, z):
-    return sqrt(x**2, y**2, z**2)
+    return sqrt(x**2 + y**2 + z**2)
 
-def get_magnitude(axes):
+def get_magnitude_dict(axes):
     return get_magnitude(axes['x'], axes['y'], axes['z'])
     
 def read_acceleration_to_dict(adxl345):
@@ -27,7 +29,8 @@ def send_alarm():
 if __name__ == "__main__":
     adxl345 = ADXL345()
     while True:
-        magnitude = get_magnitude(read_acceleration_to_dict(adxl345))
+        sleep(PERIOD_BETWEEN_SAMPLES)
+        magnitude = get_magnitude_dict(read_acceleration_to_dict(adxl345))
         send_magnitude(magnitude)
         if check_magnitude(magnitude):
             send_alarm()
