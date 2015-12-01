96190d04b15e2b329eed29ff90f567a774c3d76c
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2015-11-30 21:57:13 -0500

    added Propoer communications between STUB devices

--- ServerStandAlone/__STUB__dummyServer.py
+++ ServerStandAlone/__STUB__dummyServer.py
@@ -30,7 +30,7 @@ class ServerController():
 
 	def __init__(self, ip, port_Listen, port_Send):
 		"""
-		Function:	
+		Function:
 		To initialize the instance variables
 
 		Input arguments:
@@ -50,9 +50,9 @@ class ServerController():
 
 
 
-	def network_handler(self,conn,received_data,(received_ip,received_port)): 
+	def network_handler(self,conn,received_data,(received_ip,received_port)):
 		"""
-		Function:	
+		Function:
 		Serve the requests that is received by Phone or wearable
 
 		Input arguments:
@@ -62,79 +62,60 @@ class ServerController():
 
 
 		data_decoded = json.loads(received_data)
-		
+
 		if data_decoded['id'] == "phone":
 			print (str(time.ctime())+"Phone data Received from "+str(received_ip))
 
 			if data_decoded['command'] == 'getLatestData':
 				print ('\t'+"Sending latest Data")
 
-			
+
 				response = {'id':'server','command':'putLatestData','data':{'pulse':random.randint(50,160),'accell':random.randint(0,10)}}
 				conn.sendto(json.dumps(response, (received_ip,self.DEF_SEND_PORT)))
-				
 
-			elif data_decoded['command'] == 'getPulseDataSet':
+
+			elif data_decoded['command'] == 'getPastDataSet':
 				print ('\t'+"Sending past Data")
 				currTime = time.time();
 
 				dataSet = {}
 				for i in range (currTime- data_decoded['data'], currTime):
-					dataSet[i] = random.randint(50,160)
+					dataSet[i] = [random.randint(50,160),random.randint(0,5)]
 
-				response = {'id':'server','command':'putPulseDataSet','data':dataSet}
+				response = {'id':'server','command':'putPastDataSet','data':dataSet}
 				conn.sendto(json.dumps(response), (received_ip,self.DEF_SEND_PORT))
-				
 
-			elif data_decoded['command'] == 'getAccellDataSet':
-				print ('\t'+"Sending past Data")
-				currTime = time.time();
-
-				dataSet = {}
-				for i in range (currTime- data_decoded['data'], currTime):
-					dataSet[i] = random.randint(-2,5)
-
-				response = {'id':'server','command':'putPulseDataSet','data':dataSet}
-				conn.sendto(json.dumps(response), (received_ip,self.DEF_SEND_PORT))
-				
 
 			elif data_decoded['command'] == 'addEmergencyContact':
 				print ('\t'+"Adding Emergency Data")
-				
 
-			
+
+
 			elif data_decoded['command'] == 'getEmergencyContact':
 				print ('\t'+"Adding Emergency Data")
-				
-				
+
+
 				dataSet = 	{	'1':{'name':'Nate','phone':'6281937281' , 'email':'gg@gg.com'},
-								'2':{'name':'Gabe','phone':'6243433242' , 'email':'gg@dsadasdgg.com'} 
+								'2':{'name':'Gabe','phone':'6243433242' , 'email':'gg@dsadasdgg.com'}
 							}
-				
+
 				response = {'id':'server','command':'putEmergencyData','data':dataSet }
 
 				conn.sendto(json.dumps(response), (received_ip,self.DEF_SEND_PORT))
-				
+
 
 
 			elif data_decoded['command'] == 'remEmergencyContact':
 				print ('\t'+"Removing Emergency Data")
-				
+			else:
+				print('\t Unknown command')
+
 
 		elif data_decoded['id'] == "wearable":
 			print (str(time.ctime())+"Wearable data Received from "+str(received_ip))
 
-			if data_decoded['command'] == 'addPulseData':
+			if data_decoded['command'] == 'addSensorData':
 				print ('\t'+"Adding pulse data to database")
-
-
-			elif data_decoded['command'] == 'addRespData':
-				print ('\t'+"Adding respiratory data to database")
-
-				
-			elif data_decoded['command'] == 'addAccelData':
-				print ('\t'+"Adding accelerometer data to database")
-
 
 			elif data_decoded['command'] == 'truePositiveAlarm':
 				print ('\t'+"Adding True Positive Alarm to database")
@@ -142,20 +123,21 @@ class ServerController():
 
 			elif data_decoded['command'] == 'falsePositiveAlarm':
 				print ('\t'+"Adding False Positive Alarm to database")
+
 			elif data_decoded['command'] == 'getPatientInfo':
 				dataSet = {'name':'George', 'averageHeartRate':random.randint(60,80)}
 
 				response = {'id':'server','command':'putPatientInfo','data':dataSet}
 				conn.sendto(json.dumps(response), (received_ip,self.DEF_SEND_PORT))
-				
+
 
 
 
 	def run_server(self):
 		"""
-		Function:	
+		Function:
 		Coordinates incoming network requests to the server
-		
+
 		Input arguments:
 		None
 
@@ -165,24 +147,24 @@ class ServerController():
 
 		server = UDPFunc.createUDPSocket(self.DEF_LISTEN_IP,self.DEF_LISTEN_PORT)
 
-		
-		print("Server STUB Created")	
+
+		print("Server STUB Created")
 		try:
 			while True:
-				
+
 				#Accept each communication
 				data, addr = UDPFunc.recvUDP(server)
 				#Create a new thread for each connection that is made
 				thread = Thread(target = self.network_handler, args = (server,data,addr))
 				thread.start()
 		except (KeyboardInterrupt, SystemExit):
-			closeTCP(conn) 
+			closeTCP(conn)
 			server.close()
 
 
 def main():
 	databse_path = 'lifeBandDB.db'
-	listen_ip ='0.0.0.0'#'172.17.144.192' 
+	listen_ip ='0.0.0.0'#'172.17.144.192'
 	listen_port = 5005
 	send_port = 6006
 
